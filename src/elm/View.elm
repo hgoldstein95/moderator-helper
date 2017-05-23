94c1951d4c8d6de406917ab43e2242794e9ca154
Harrison Goldstein <hgoldstein95@gmail.com>
2017-05-22 23:39:46 -0400

    Added mdl styles to night round

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -3,14 +3,15 @@ module View exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode as Json
 import Dict exposing (Dict)
 import Material.Scheme
-import Material.Chip as Chip
 import Material.Button as Button
 import Material.Options as Options
 import Material.Color as Color
 import Material.List as Lists
 import Material.Icon as Icon
+import Material.Typography as Typo
 import Model exposing (..)
 import Update exposing (..)
 import Mafia exposing (..)
@@ -75,9 +76,9 @@ roleInput model role =
 
 viewCreate : Model -> Html Msg
 viewCreate model =
-    div []
-        [ h2 [] [ text "Create" ]
-        , div [] (List.map (roleInput model) (Dict.values roles))
+    Options.div []
+        [ Options.styled p [ Typo.display2 ] [ text "Create a Setup" ]
+        , Options.div [] (List.map (roleInput model) (Dict.values roles))
         , Button.render Mdl
             []
             model.mdl
@@ -86,79 +87,59 @@ viewCreate model =
         ]
 
 
+makeActBtn : Model -> Player -> Action -> Html Msg
+makeActBtn model p a =
+    Button.render Mdl
+        []
+        model.mdl
+        [ Options.onWithOptions
+            "click"
+            { stopPropagation = True
+            , preventDefault = False
+            }
+            (Json.succeed (Act p a))
+        , case model.visiting of
+            Nothing ->
+                Options.nop
+
+            Just ( play, act ) ->
+                if p.id == play.id && act == a then
+                    Button.colored
+                else
+                    Options.nop
+        ]
+        [ text (toString a) ]
+
+
 playerItem : Model -> Player -> Html Msg
 playerItem model p =
-    let
-        actBtns =
-            List.map
-                (\a ->
-                    button
-                        [ class "role-modifier"
-                        , onClick (Act p a)
-                        ]
-                        [ text (toString a) ]
-                )
-                p.role.actions
-
-        isVisiting =
-            case model.visiting of
-                Nothing ->
-                    False
-
-                Just ( play, _ ) ->
-                    p.id == play.id
-
-        attrs =
-            onClick (Visit p)
-                :: (if isVisiting then
-                        [ class "visiting" ]
-                    else
-                        []
-                   )
-    in
-        li
-            [ class "role-item" ]
-            [ div []
-                ([ span
-                    attrs
-                    [ text <| (toString p.id) ++ " " ++ p.role.name ]
-                 ]
-                    ++ actBtns
-                )
-            ]
-
-
-showVisited : Model -> Html Msg
-showVisited model =
-    ul
-        [ class "role-list" ]
-        (List.concatMap
-            (\( id, l ) ->
-                List.map
-                    (\( s, a ) ->
-                        li []
-                            [ text <|
-                                (toString s.id)
-                                    ++ " -"
-                                    ++ (toString a)
-                                    ++ "-> "
-                                    ++ (toString id)
-                            ]
-                    )
-                    l
-            )
-            (Dict.toList model.visited)
-        )
+    Lists.li
+        [ Options.onClick (Visit p) ]
+        [ Lists.content []
+            [ span [] [ text <| (toString p.id) ++ " " ++ p.role.name ] ]
+        , Lists.content2 [] (List.map (makeActBtn model p) p.role.actions)
+        ]
 
 
 viewNight : Model -> Html Msg
 viewNight model =
     div []
-        [ h2 [] [ text "Night" ]
-        , button [ onClick ToDay ] [ text "In the morning..." ]
-        , button [ onClick EndGame ] [ text "Game Over" ]
-        , ul [] (List.map (playerItem model) (Dict.values model.players))
-        , showVisited model
+        [ Options.styled p [ Typo.display2 ] [ text "Night Round" ]
+        , Lists.ul [] (List.map (playerItem model) (Dict.values model.players))
+        , Button.render Mdl
+            []
+            model.mdl
+            [ Options.onClick ToDay
+            , Button.primary
+            , Button.raised
+            , Options.css "margin-right" "20px"
+            ]
+            [ text "In the morning..." ]
+        , Button.render Mdl
+            []
+            model.mdl
+            [ Options.onClick EndGame, Button.accent, Button.raised ]
+            [ text "Game Over" ]
         ]
 
 
